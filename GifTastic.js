8b0c8d7f27ada7b017ce99539f0b6479751b3dcc
Maliah Lacy <mlacy0915@gmail.com>
2019-10-01 19:23:44 -0500

    incomplete files added

---
+++ GifTastic.js
@@ -0,0 +1,7 @@
+var characters = ["Chucky Finster", "Hello Kitty", "Tweety", "Bart Simpson", "Stewie Griffin", "Jessica Rabbit", Angelica Pickles"]
+        function renderButtons() {
+            $("#buttons-view").empty();
+            for (var i = 0; i < movies.length; i++) {
+                var a = $("<button>");
+            }
+        }
