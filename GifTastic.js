c510e1acc50279dc85ce332458fcfac416b17a1a
Maliah Lacy <mlacy0915@gmail.com>
2020-01-02 22:46:15 -0600

    added ready function and on click

--- GifTastic.js
+++ GifTastic.js
@@ -1,7 +1,13 @@
-var characters = ["Chucky Finster", "Hello Kitty", "Tweety", "Bart Simpson", "Stewie Griffin", "Jessica Rabbit", Angelica Pickles"]
+var characters = ["Chucky Finster", "Hello Kitty", "Tweety", "Bart Simpson", "Stewie Griffin", "Jessica Rabbit", "Angelica Pickles"]
         function renderButtons() {
             $("#buttons-view").empty();
             for (var i = 0; i < movies.length; i++) {
                 var a = $("<button>");
             }
         }
+$(document).ready(function(){
+    $ ("#AddCharacter").onclick("click", function() {
+        alert("Character Added!")
+    });
+    
+});
